1ca2a054eac3d7c8f3387f5021cd130f60cd3c6f
sdoering <code@schriftrolle.de>
2016-02-14 10:12:01 +0100

    til time.js 2016/02/14

---
+++ web/js/time.js
@@ -0,0 +1,30 @@
+var now = new Date();
+
+$.each({
+  utc: {
+    hours: now.getUTCHours(),
+    minutes: now.getUTCMinutes()
+  },
+  local: {
+    hours: now.getHours(),
+    minutes: now.getMinutes()
+  }
+}, function (type, values) {
+
+  var minutes = values.minutes;
+  var hours = values.hours;
+  var meridiem;
+
+  if (hours > 12) {
+    hours = hours % 12;
+    meridiem = 'PM';
+  } else {
+    meridiem = 'AM';
+  }
+
+  hours = hours || 12; // hour '0' should be '12'
+
+  trackingService.pushToDataLayer('dsdSection.' + type + 'Time', hours + ':' + minutes + ' ' + meridiem);
+
+});
+// learned thanks to https://github.com/thoras
